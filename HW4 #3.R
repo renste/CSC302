52d7f28265cc3c44bfa2e24bbcac5794468e42a1
renste <renste@umich.edu>
2024-08-02 21:50:36 -0400

    Add files via upload

---
+++ HW4 #3.R
@@ -0,0 +1,48 @@
+library(MASS)
+library(ggplot2)
+
+cars93 <- MASS::Cars93
+
+# LM method
+p1 <- ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "lm", formula = y ~ x, color = "#8fe388") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)") +
+  ggtitle("LM Method") +
+  theme(plot.title = element_text(size = 14, color = "#8fe388"))
+
+# GLM method
+p2 <- ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "glm", formula = y ~ x, color = "#fe8d6d") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)") +
+  ggtitle("GLM Method") +
+  theme(plot.title = element_text(size = 14, color = "#fe8d6d"))
+
+# GAM method
+p3 <- ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "gam", formula = y ~ x, color = "#7c6bea") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)") +
+  ggtitle("GAM Method") +
+  theme(plot.title = element_text(size = 14, color = "#7c6bea"))
+
+# Display the plots
+library(gridExtra)
+grid.arrange(p1, p2, p3, nrow = 3)
+
